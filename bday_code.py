41423bd12622adf05566cdbe13b2c50fe8393cb9
Gaurav <singh.gaurav2712@gmail.com>
2022-02-14 17:37:02 +0530

    modified:   bday_code.py modified:   cards.py

--- bday_code.py
+++ bday_code.py
@@ -1,19 +1,16 @@
 import time
 import schedule
-
 import json
 import os
 import csv
 import datetime
 import cards
-
-
-
 from webexteamssdk import WebexTeamsAPI
 
 access_token = os.environ.get('WEBEX_TEAMS_ACCESS_TOKEN')
+# MDJhMDgxZjctNzdmNy00NDIyLWEyZDQtMmQzYmI4NzM2MzI5NzYxNzY5MDctYWFm_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f
 roomID = os.environ.get('ROOM_ID')
-
+#
 api = WebexTeamsAPI(access_token= access_token)
 
 
@@ -28,7 +25,6 @@ farewell_list = []
 #Function to check Birthday's by reading the csv
 def checkTodaysBirthdays():
     print("Checking Bdays...")
-    #fileName = open(birthdayFile, 'r')
     isbday = False
     today = datetime.datetime.now()
     month = today.month
@@ -38,16 +34,8 @@ def checkTodaysBirthdays():
     bday = date + "-" + month_name
     stri = "hi"
     flag = 0
-
-    #csv_file = csv.reader(open(test_file, "r"), delimiter=",")
-
-
-
     with open('bdaysfile.json') as bdays_file_json:
         bday_file= json.load(bdays_file_json)
-
-
-#loop through the csv list
     for row in bday_file:
 
         #if current rows 2nd value is equal to input, print that row
@@ -60,11 +48,6 @@ def checkTodaysBirthdays():
             })
             reminder_to_space(row)
 
-        #else :
-            #time.sleep(60)
-            #checkTodaysBirthdays()
-
-
     if(isbday):
         time.sleep(30)
         bday_wish(bday_list,isbday)
@@ -73,55 +56,6 @@ def checkTodaysBirthdays():
             time.sleep(60)
             print("No Bdays")
             checkTodaysBirthdays()
-
-
-# def checkTodaysFarwells():
-#     print("Checking Farewells...")
-#     #fileName = open(birthdayFile, 'r')
-#     isfarewell = False
-#     today = datetime.datetime.now()
-#     month = today.month
-
-#     month_name = month_dict[month]
-#     date = str(today.day)
-#     bday = date + "-" + month_name
-
-
-
-
-#     with open('bdaysfile.json') as bdays_file_json:
-#         bday_file= json.load(bdays_file_json)
-
-
-# #loop through the csv list
-#     for row in bday_file:
-
-#         #if current rows 2nd value is equal to input, print that row
-#         if bday == row["Birthday"]:
-#             isfarewell = True
-#             farewell_list.append({
-#             'Name' : row["Employee Name"],
-#             'email' : row["email"],
-
-#             })
-#             reminder_to_space(row)
-
-#         #else :
-#             #time.sleep(60)
-#             #checkTodaysBirthdays()
-
-
-#     if(isfarewell):
-#         time.sleep(30)
-#         farewell_wish(farewell_list,isfarewell)
-
-#     else :
-#             time.sleep(60)
-#             print("No Farewells today")
-#             checkTodaysFarwells()
-
-
-
 
 def reminder_to_space(bday):
      #Reminder to space
@@ -135,9 +69,6 @@ def reminder_to_space(bday):
                         "content": REMINDER_CARD
                         }],
                     )
-
-
-
 def bday_wish(bday_list,isbday) :
     f = open("Logs.txt", "a")
     if(isbday):
@@ -224,95 +155,7 @@ def bday_wish(bday_list,isbday) :
 
     f.close()
 
-# def farewell_wish(farewell_list,isfarewell):
-#     f = open("Logs.txt", "a")
-#     if(isfarewell):
-
-#         for i in range(0,len(farewell_list)):
-
-#             CARD_CONTENT_TO_FAREWELL_PERSON = cards.CARD_CONTENT_TO_FAREWELL_PERSON
-#             CARD_CONTENT_TO_EVERYONE_FAREWELL = cards.CARD_CONTENT_TO_EVERYONE_FAREWELL
-
-#             SUCCESS_CARD = cards.SUCCESS_CARD
-
-#             #print(bday_list[i]['Name'])
-
-#             name=farewell_list[i]['Name']
-#             email=farewell_list[i]['email']
-#             farewell_text_to_everyone = "It's " + name +"'s last day in our team."
-#             farewell_text_to_person= "Hey "+name+","
-#             CARD_CONTENT_TO_FAREWELL_PERSON["body"][1]["text"]=farewell_text_to_person
-
-#             #sending message to bday person
-#             f.write("\nCard sent to "+ farewell_list[i]['email'])
-#             print("Its "+farewell_list[i]['email']+"'s last day in our team")
-#             api.messages.create(toPersonEmail=farewell_list[i]['email'],
-#             text="Farewell",
-#             attachments=[{
-#                     "contentType": "application/vnd.microsoft.card.adaptive",
-#                     "content": CARD_CONTENT_TO_FAREWELL_PERSON
-#                     }],)
-
-
-#             #Bday notification to everyone
-#             with open('bdaysfile.json') as bdays_file_json:
-#                 bday_file= json.load(bdays_file_json)
-
-#             count_checker = 0
-#             f.write("Logs for "+ farewell_list[i]['Name']+"'s Farewell")
-#             for row in bday_file:
-
-
-#                 #print(row[3])
-#                 if row["Employee Name"]!=farewell_list[i]['Name'] and row["email"]!="email":
-#                     count_checker+=1
-#                     print(count_checker)
-#                     print(row["email"])
-
-#                     recipient_name = row["Employee Name"]
-#                     recipient_name_text = "Hey " + recipient_name + ","
-#                     CARD_CONTENT_TO_EVERYONE_FAREWELL["body"][1]["text"]= recipient_name_text
-#                     CARD_CONTENT_TO_EVERYONE_FAREWELL["body"][2]["text"]=farewell_text_to_everyone
-#                     #print(CARD_CONTENT_TO_EVERYONE["body"][1]["text"])
-#                     api.messages.create(toPersonEmail=row["email"],
-#                                         text=farewell_text_to_everyone,
-#                                         attachments=[{
-#                                                         "contentType": "application/vnd.microsoft.card.adaptive",
-#                                                         "content": CARD_CONTENT_TO_EVERYONE_FAREWELL
-#                             }],
-#                                         )
-
-#                     f.write("\nCard sent to "+ row["email"])
-#             #Successful card
-#             print(count_checker)
-#             if count_checker == len(bday_file) - len(bday_list):
-#                 completed_text =  name=bday_list[i]['Name']+"'s Farewell reminders successfully sent to "+ str(count_checker) + " people."
-#                 SUCCESS_CARD["body"][1]["text"]=completed_text
-#                 api.messages.create(roomId= roomID,text=completed_text,
-#                 attachments=[{
-#                                                         "contentType": "application/vnd.microsoft.card.adaptive",
-#                                                         "content": SUCCESS_CARD
-#                             }],
-#                                         )
-
-#     f.close()
-
-
-
-
-
-
 #Function to schedule
 if __name__ == "__main__":
-    # comment this when you scheduler is running else bday person will
-    #receive 2 messages
     checkTodaysBirthdays()
-    #checkTodaysFarwells()
-
     print("Starting Exection ....")
-    #schedule.every().day.at("09:27").do(checkTodaysBirthdays)
-    #schedule.every().day.at("23:44").do(bday_wish)
-
-    #while True:
-       #schedule.run_pending()
-       #time.sleep(60) # wait one minute
--- cards.py
+++ cards.py
@@ -2,11 +2,11 @@ CARD_CONTENT_TO_BDAY_PERSON = {
     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
     "type": "AdaptiveCard",
     "version": "1.1",
-    "body": 
+    "body":
     [
     {
     "type": "ColumnSet",
-    "columns": 
+    "columns":
         [
             {
             "type": "Column",
@@ -15,7 +15,6 @@ CARD_CONTENT_TO_BDAY_PERSON = {
                             "type": "Image",
                             "style": "Person",
                             "url": "https://icons.iconarchive.com/icons/mohsenfakharian/christmas/512/cake-icon.png",
-                            #"url":"https://www.sampleposts.com/wp-content/uploads/2020/04/Farewell-Quotes-For-Final-Year-Students.png",
                             "size": "Medium",
                             "height": "75px"
                         }
@@ -24,70 +23,70 @@ CARD_CONTENT_TO_BDAY_PERSON = {
             }
         ]
     },
-                    
-                 
-   
+
+
+
         {
             "type": "TextBlock",
             "text": "",
             "size": "medium",
             "weight": "light",
             "color": "Accent"
-                        
+
         },
           {
             "type": "TextBlock",
             "text": "Happy Birthday! Wishing you a day filled with happiness and a year filled with joy.",
             "weight": "bolder",
             "wrap": True
-            
-                        
+
+
         },
-        
-        
+
+
         {
-            
+
             "type": "TextBlock",
-            "text": "Check out the wishes from your teammates on the Collab Birthday Board",
+            "text": "Check out the wishes from your teammates on the A2O Birthday Board",
             "spacing":"small",
             "weight":"Light",
             "wrap": True
-        
+
         },
-            
+
         {
-            
+
             "type": "TextBlock",
-            "text": "YOUR COLLAB FAMILY",
+            "text": "YOUR A2O FAMILY",
             "weight": "Lighter",
             "color": "Accent",
             "wrap": True
-        
+
         }
 
     ],
-     
+
         "actions": [
         {
             "type": "Action.OpenUrl",
-            "title": "Collab Birthday Board",
+            "title": "A2O Birthday Board",
             "id": "",
             "style": "positive",
-            "url": "https://ciscocx.kudoboard.com/boards/PCj6BVyD"
+            "url": "https://ciscocx.kudoboard.com/"
         }
 ]
-    
+
 }
 
 CARD_CONTENT_TO_EVERYONE = {
     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
     "type": "AdaptiveCard",
     "version": "1.1",
-    "body": 
+    "body":
     [
     {
     "type": "ColumnSet",
-    "columns": 
+    "columns":
            [
                 {
                    "type": "Column",
@@ -111,310 +110,50 @@ CARD_CONTENT_TO_EVERYONE = {
                     "size": "medium",
                     "weight": "light",
                     "color": "Accent"
-   
+
                  },
-        
+
                 {
-            
+
                     "type": "TextBlock",
                     "text": "",
                     "wrap": True,
                     "weight":"bolder",
                     "spacing":"small"
-                
-        
+
+
                 },
                 {
-            
+
                     "type": "TextBlock",
-                    "text": "Send your wishes on the Collab Birthday Board!",
+                    "text": "Send your wishes on the A2O Birthday Board!",
                     "wrap": True,
                     "weight": "Light",
                     "spacing":"small"
-            
-        
+
+
                 },
                 {
-            
+
                 "type": "TextBlock",
-                "text": "YOUR COLLAB FAMILY",
+                "text": "YOUR A2O FAMILY",
                 "weight": "Lighter",
                 "color": "Accent",
                 "wrap": True
-        
+
                 }
     ],
-    
+
             "actions": [
                 {
                     "type": "Action.OpenUrl",
-                    "title": "Collab Birthday Board",
+                    "title": "A2O Birthday Board",
                     "id": "",
                     "style": "positive",
-                    "url": "https://ciscocx.kudoboard.com/boards/PCj6BVyD"
+                    "url": "https://ciscocx.kudoboard.com/"
                 }
                 ]
 }
-
-CARD_CONTENT_TO_FAREWELL_PERSON ={
-     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-    "type": "AdaptiveCard",
-    "version": "1.1",
-    "body": 
-    [
-    {
-    "type": "ColumnSet",
-    "columns": 
-        [
-            {
-            "type": "Column",
-                "items": [
-                        {
-                            "type": "Image",
-                            "style": "Person",
-                            #"url": "https://icons.iconarchive.com/icons/mohsenfakharian/christmas/512/cake-icon.png",
-                            "url":"https://www.sampleposts.com/wp-content/uploads/2020/04/Farewell-Quotes-For-Final-Year-Students.png",
-                            "size": "Medium",
-                            "height": "75px"
-                        }
-                    ],
-                "width": "auto"
-            }
-        ]
-    },
-                    
-                 
-   
-        {
-            "type": "TextBlock",
-            "text": "",
-            "size": "medium",
-            "weight": "light",
-            "color": "Accent"
-                        
-        },
-          {
-            "type": "TextBlock",
-            #"text": "Happy Birthday! Wishing you a day filled with happiness and a year filled with joy.",
-            "text": "Thank you for your hard work and dedication. ",
-            "weight": "bolder",
-            "wrap": True
-            
-                        
-        },
-          {
-            "type": "TextBlock",
-            #"text": "Happy Birthday! Wishing you a day filled with happiness and a year filled with joy.",
-            "text": "Best of luck on your new journey. We will miss you! ",
-            "weight": "bolder",
-            "wrap": True
-            
-                        
-        },
-        
-        
-        {
-            
-            "type": "TextBlock",
-            "text": "Check out the wishes from your teammates on the Collab Farewell Board",
-            "spacing":"small",
-            "weight":"Light",
-            "wrap": True
-        
-        },
-            
-        {
-            
-            "type": "TextBlock",
-            "text": "YOUR COLLAB FAMILY",
-            "weight": "Lighter",
-            "color": "Accent",
-            "wrap": True
-        
-        }
-
-    ],
-     
-        "actions": [
-        {
-            "type": "Action.OpenUrl",
-            "title": "Collab Farewell Board",
-            "id": "",
-            "style": "positive",
-            "url": "https://ciscocx.kudoboard.com/boards/tKk71bf2"
-        }
-]
-
-}
-
-CARD_CONTENT_TO_EVERYONE_FAREWELL = {
-    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-    "type": "AdaptiveCard",
-    "version": "1.1",
-    "body": 
-    [
-    {
-    "type": "ColumnSet",
-    "columns": 
-           [
-                {
-                   "type": "Column",
-                    "items": [
-                        {
-                            "type": "Image",
-                            "style": "Person",
-                            #"url": "https://icons.iconarchive.com/icons/mohsenfakharian/christmas/512/cake-icon.png",
-                            "url":"https://www.sampleposts.com/wp-content/uploads/2020/04/Farewell-Quotes-For-Final-Year-Students.png",
-                            "size": "Medium",
-                            "height": "75px"
-                        }
-                ],
-                    "width": "auto"
-                }
-            ]
-        },
-                {
-                    "type": "TextBlock",
-                    "text": "",
-                    "size": "medium",
-                    "weight": "light",
-                    "color": "Accent"
-   
-                 },
-        
-                {
-            
-                    "type": "TextBlock",
-                    "text": "",
-                    "wrap": True,
-                    "weight":"bolder",
-                    "spacing":"small"
-                
-        
-                },
-                {
-            
-                    "type": "TextBlock",
-                    "text": "Send your wishes on the Collab Farewell Board!",
-                    "wrap": True,
-                    "weight": "Light",
-                    "spacing":"small"
-            
-        
-                },
-                {
-            
-                "type": "TextBlock",
-                "text": "YOUR COLLAB FAMILY",
-                "weight": "Lighter",
-                "color": "Accent",
-                "wrap": True
-        
-                }
-    ],
-    
-            "actions": [
-                {
-                    "type": "Action.OpenUrl",
-                    "title": "Collab Farewell Board",
-                    "id": "",
-                    "style": "positive",
-                    "url": "https://ciscocx.kudoboard.com/boards/tKk71bf2"
-                }
-                ]
-
-}
-            
-
-# CARD_CONTENT_TO_EVERYONE = {
-#     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-#     "type": "AdaptiveCard",
-#     "version": "1.1",
-#     "body": 
-#     [
-#     {
-#     "type": "ColumnSet",
-#     "columns": 
-#            [
-#                 {
-#                    "type": "Column",
-#                     "items": [
-#                         {
-#                             "type": "Image",
-#                             "style": "Person",
-#                             #"url": "https://icons.iconarchive.com/icons/mohsenfakharian/christmas/512/cake-icon.png",
-#                             #"url":"https://www.sampleposts.com/wp-content/uploads/2020/04/Farewell-Quotes-For-Final-Year-Students.png",
-#                             "url":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL6RQQHGjxgeu5nXaiwF60ESPyKdYqMAoU6A&usqp=CAU",
-#                             "size": "Medium",
-#                             "height": "75px"
-#                         }
-#                 ],
-#                     "width": "auto"
-#                 }
-#             ]
-#         },
-#                 {
-#                     "type": "TextBlock",
-#                     "text": "",
-#                     "size": "medium",
-#                     "weight": "light",
-#                     "color": "Accent"
-   
-#                  },
-        
-#                 {
-            
-#                     "type": "TextBlock",
-#                     "text": "Team employee engagement would like to celebrate your ",
-#                     "weight":"bolder",
-#                     "spacing":"small"
-                
-        
-#                 },
-#                  {
-            
-#                     "type": "TextBlock",
-#                     "text": "special days with the team. ",
-#                     "weight":"bolder",
-#                     "spacing":"small"
-                
-        
-#                 },
-#                  {
-            
-#                     "type": "TextBlock",
-#                     "text": "Our records indicate that your information is not updated. ",
-#                     "weight":"bolder",
-#                     "spacing":"small"
-                
-        
-#                 },
-#                 {
-            
-#                     "type": "TextBlock",
-#                     "text": "Please fill out the below survey to update your information.",
-#                     "wrap": True,
-#                     "wrap": True,
-#                     "weight": "Light",
-#                     "spacing":"small"
-            
-        
-#                 },
-                
-            
-                
-#     ],
-    
-#             "actions": [
-#                 {
-#                     "type": "Action.OpenUrl",
-#                     "title": "Survey Form",
-#                     "id": "",
-#                     "style": "positive",
-#                     "url": "https://app.smartsheet.com/b/form/c8e0dcbcbe97470f8009f61491f1e4a3"
-#                 }
-#                 ]
-# }
 
 REMINDER_CARD = {
     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
@@ -428,7 +167,7 @@ REMINDER_CARD = {
         "type": "Column",
                     "items": [
                         {
-                        
+
                             "type": "Image",
                             "style": "Person",
                             "url": "https://icons.iconarchive.com/icons/blackvariant/button-ui-microsoft-office-apps/1024/Microsoft-Reminders-icon.png",
@@ -446,21 +185,21 @@ REMINDER_CARD = {
                     "size": "medium",
                     "weight": "light",
                     "color": "Accent"
-   
+
        },
-        
+
         {
-            
+
             "type": "TextBlock",
             "text": "",
             "wrap": True,
             "weight":"bolder",
             "spacing":"small"
-            
-        
+
+
         }]
-                    
-   
+
+
 }
 SUCCESS_CARD = {
     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
@@ -490,9 +229,9 @@ SUCCESS_CARD = {
                 "text": "",
                 "weight": "light",
                 "color": "Accent"
-   
+
        }
     ]
-                    
-   
+
+
 }
